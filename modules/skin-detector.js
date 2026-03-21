f00fc817e6e6a49f45ffacf211cc7ef7473259ea
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2026-03-21 00:07:06 -0600

    realiza una mejor deteccion de colores - RGB + YCbCr. sigue teniendo fallos al localizar colores.

--- modules/skin-detector.js
+++ modules/skin-detector.js
@@ -3,12 +3,12 @@ export function detectSkinTone(image){
 const canvas = document.createElement("canvas")
 const ctx = canvas.getContext("2d")
 
-canvas.width = 50
-canvas.height = 50
+canvas.width = 60
+canvas.height = 60
 
-ctx.drawImage(image,0,0,50,50)
+ctx.drawImage(image,0,0,60,60)
 
-const data = ctx.getImageData(0,0,50,50).data
+const data = ctx.getImageData(0,0,60,60).data
 
 let skinPixels = []
 
@@ -18,24 +18,36 @@ const r = data[i]
 const g = data[i+1]
 const b = data[i+2]
 
-/* regla simple de piel */
+/* convertir a YCbCr */
 
-if(
-r > 95 &&
+const y  =  0.299*r + 0.587*g + 0.114*b
+const cb = 128 - 0.168736*r - 0.331364*g + 0.5*b
+const cr = 128 + 0.5*r - 0.418688*g - 0.081312*b
+
+/* rango típico de piel */
+
+const isSkinYCbCr =
+cb >= 77 && cb <= 127 &&
+cr >= 133 && cr <= 173
+
+/* filtro adicional RGB */
+
+const isSkinRGB =
+r > 60 &&
 g > 40 &&
 b > 20 &&
 r > g &&
-r > b &&
-Math.abs(r-g) > 15
-){
+Math.abs(r - g) > 10
 
+if(isSkinYCbCr && isSkinRGB){
 skinPixels.push({r,g,b})
-
 }
 
 }
 
-if(skinPixels.length < 20){
+/* no suficiente piel */
+
+if(skinPixels.length < 30){
 return null
 }
 
