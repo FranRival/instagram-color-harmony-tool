e55b681b6c871c83cfe4a58d32432c4a77bc413d
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2026-03-21 00:16:20 -0600

    3 filtros agregados: Dominancia de rojo RGB - eliminar colores frios B es alto, no es piel - saturacion minimal real evitar colores raros o artificiales. - no funcional. Detecta aun colores azules.

--- modules/skin-detector.js
+++ modules/skin-detector.js
@@ -18,40 +18,61 @@ const r = data[i]
 const g = data[i+1]
 const b = data[i+2]
 
-/* convertir a YCbCr */
+/* ===== convertir a YCbCr ===== */
 
 const y  =  0.299*r + 0.587*g + 0.114*b
 const cb = 128 - 0.168736*r - 0.331364*g + 0.5*b
 const cr = 128 + 0.5*r - 0.418688*g - 0.081312*b
 
-/* rango típico de piel */
+/* ===== rango típico de piel ===== */
 
 const isSkinYCbCr =
 cb >= 77 && cb <= 127 &&
 cr >= 133 && cr <= 173
 
-/* filtro adicional RGB */
+/* ===== filtro RGB base ===== */
 
 const isSkinRGB =
 r > 60 &&
 g > 40 &&
-b > 20 &&
-r > g &&
-Math.abs(r - g) > 10
+b > 20
 
-if(isSkinYCbCr && isSkinRGB){
+/* ===== filtro final (clave) ===== */
+
+const isSkinFinal =
+isSkinYCbCr &&
+isSkinRGB &&
+
+/* rojo dominante */
+
+r > g &&
+g > b &&
+
+/* evitar azules */
+
+b < 0.9 * g &&
+
+/* diferencia rojo-azul */
+
+(r - b) > 20 &&
+
+/* evitar saturación artificial */
+
+(r - g) < 100
+
+if(isSkinFinal){
 skinPixels.push({r,g,b})
 }
 
 }
 
-/* no suficiente piel */
+/* ===== no suficiente piel ===== */
 
 if(skinPixels.length < 30){
 return null
 }
 
-/* promedio */
+/* ===== promedio ===== */
 
 let r=0,g=0,b=0
 
